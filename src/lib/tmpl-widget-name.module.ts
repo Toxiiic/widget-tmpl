6ce56a74e5b0b3b77e2fc1527b87b3636abeca05
Toxiiic <872435708@qq.com>
2020-04-14 11:04:24 +0800

    feat: 元部件 module 增加国际化库 module 引用

--- src/lib/tmpl-widget-name.module.ts
+++ src/lib/tmpl-widget-name.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { TmplWidgetNameComponent } from './tmpl-widget-name.component';
 import { WidgetTitleBarModule } from '@gspwidget/widget-devkit';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [TmplWidgetNameComponent],
   imports: [
-    WidgetTitleBarModule
+    CommonModule,
+    WidgetTitleBarModule,
+    TranslateModule.forChild()
   ],
   exports: [TmplWidgetNameComponent]
 })
